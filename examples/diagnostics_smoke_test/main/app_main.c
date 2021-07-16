c1d2bd4a75ed205da9fba3285f71260d6da88043
Shubham Patil <shubham.patil@espressif.com>
2021-07-16 17:28:19 +0530

    smoke_test: Tests for metrics and variables verification

--- examples/diagnostics_smoke_test/main/app_main.c
+++ examples/diagnostics_smoke_test/main/app_main.c
@@ -12,9 +12,10 @@
 #include "esp_insights.h"
 #include "esp_rmaker_utils.h"
 #include "app_wifi.h"
-
+#include "esp_diagnostics_system_metrics.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
 
 /* The examples uses configuration that you can set via project configuration menu
 
@@ -36,16 +37,21 @@
 #define EXAMPLE_DIAG_LOG_TYPE               0
 #endif /* CONFIG_DIAG_ENABLE_LOG_TYPE_ALL */
 
+#define MAX_CRASHES 5
+#define MAX_PTRS    30
+
 static const char *TAG = "diag_smoke";
+RTC_NOINIT_ATTR static uint32_t s_reset_count;
+static void *s_ptrs[MAX_PTRS];
 
 static void smoke_test(void *arg)
 {
     ESP_LOGI(TAG, "In smoke_test");
     int dice;
     int count = 0;
+    bool allocating = false;
 
     while (1) {
-        count++;
         dice = esp_random() % 500;
         ESP_LOGI(TAG, "dice=%d", dice);
         if (dice > 0 && dice < 150) {
@@ -56,11 +62,34 @@ static void smoke_test(void *arg)
             ESP_DIAG_EVENT(TAG, "[count][%d]", count);
         } else {
             /* 30 in 500 probability to crash */
-            char *p = NULL;
-            ESP_LOGE(TAG, "[count][%d] Crashing...", count);
-            assert(p == NULL);
-            assert(p != NULL);
+            if (s_reset_count > MAX_CRASHES) {
+                ESP_DIAG_EVENT(TAG, "[count][%d]", count);
+            } else {
+               ESP_LOGE(TAG, "[count][%d] [crash_count][%d] [excvaddr][0x0f] Crashing...", count, s_reset_count);
+               *(int *)0x0F = 0x10;
+           }
         }
+
+        esp_diag_heap_metrics_dump();
+        if (count % MAX_PTRS == 0) {
+            allocating = !allocating;
+            ets_printf("Allocating:%s\n", allocating ? "true" : "false");
+        }
+        if (allocating) {
+            uint32_t size = 1024 * (esp_random() % 8);
+            void *p = malloc(size);
+            if (p) {
+                memset(p, size, 'A' + (esp_random() % 26));
+                ESP_LOGI(TAG, "Allocated %d bytes", size);
+            }
+            s_ptrs[count % MAX_PTRS] = p;
+        } else {
+            free(s_ptrs[count % MAX_PTRS]);
+            s_ptrs[count % MAX_PTRS] = NULL;
+            ESP_LOGI(TAG, "Freeing some memory...");
+        }
+
+        count++;
         vTaskDelay(1000);
     }
 }
@@ -95,6 +124,12 @@ void app_main(void)
         ESP_LOGE(TAG, "Failed to init ESP Insights, err:0x%x", ret);
     }
     ESP_ERROR_CHECK(ret);
+
+    if (esp_reset_reason() == ESP_RST_POWERON)  {
+        s_reset_count = 1;
+    } else {
+        s_reset_count++;
+    }
 
     xTaskCreate(smoke_test, "smoke_test", (8 * 1024), NULL, 5, NULL);
 }
